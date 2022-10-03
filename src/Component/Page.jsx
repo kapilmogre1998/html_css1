3777a08ede1e1d64aa39a57b1e1d1fb6eb44766d
Kapil <kapil.mogre@nobroker.in>
2022-10-03 23:08:12 +0530

    implemented animation on text and image

--- src/Component/Page.jsx
+++ src/Component/Page.jsx
@@ -16,9 +16,9 @@ const StaticPage = () => {
             </ul>
            </nav>
            <div className='content' > 
-            <h1>Make <br></br> New Friends</h1>
-            <div>Socially is a new social media platform to make new friends online in the world. Now let's explore all it's amazing features</div>
-            <div>Join Now</div>
+            <h1 className='heading' >Make <br></br> New Friends</h1>
+            <div className='sub-heading' >Socially is a new social media platform to make new friends online in the world. Now let's explore all it's amazing features</div>
+            <div className='join-btn' >Join Now</div>
            </div>
            <img className='girl-img' width={420} src="https://i.postimg.cc/vHnkDdBj/pic.png" alt="" />
         </section>
